257f2d82a88dbf82e7c831712a5325e0abf09368
Sahil Ishar <sahil.ishar@gmail.com>
2015-10-24 22:10:18 -0400

    Initial Commit

---
+++ GIFSearcher/GIFDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  GIFDetailViewController.h
+//  GIFSearcher
+//
+//  Created by Sahil Ishar on 10/24/15.
+//  Copyright © 2015 Sahil Ishar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GIFDetailViewController : UIViewController
+
+@end
---
+++ GIFSearcher/GIFDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  GIFDetailViewController.m
+//  GIFSearcher
+//
+//  Created by Sahil Ishar on 10/24/15.
+//  Copyright © 2015 Sahil Ishar. All rights reserved.
+//
+
+#import "GIFDetailViewController.h"
+
+@implementation GIFDetailViewController
+
+@end
