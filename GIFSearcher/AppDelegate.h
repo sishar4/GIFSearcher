b91921a8bd7cce2e9996a62ad809404dcd369a5f
Sahil Ishar <sahil.ishar@gmail.com>
2015-10-24 21:40:33 -0400

    Initial Commit

---
+++ GIFSearcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIFSearcher
+//
+//  Created by Sahil Ishar on 10/24/15.
+//  Copyright © 2015 Sahil Ishar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIFSearcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIFSearcher
+//
+//  Created by Sahil Ishar on 10/24/15.
+//  Copyright © 2015 Sahil Ishar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
